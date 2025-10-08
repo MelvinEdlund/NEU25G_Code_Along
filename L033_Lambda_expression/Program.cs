1c2a76a3c680aa4692a55203dba3f9be31453697
Melvinedl <medlund01@gmail.com>
2025-10-08 13:27:44 +0200

    l035

---
+++ L033_Lambda_expression/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+Func<int, int> square = x => x * x;
+Action<int> printInt = i => Console.WriteLine(i);
+
+printInt(square(5));
+
+//static int Square(int x)
+//{
+//    return x * x;
+//}
+
+PrintResults(x => x * 2);
+
+Console.WriteLine();
+
+PrintResults(x => x % 2);
+
+Console.WriteLine();
+
+PrintResults(n =>
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += i;
+	}
+	return sum;
+});
+
+static void PrintResults(Func<int, int> func)
+{
+	for (int i = 1; i <= 10; i++)
+	{
+        Console.WriteLine($"{i}: {func(i)}");
+	}
+}
+
+Console.WriteLine();
+
+//Func<Person, int, bool> isLegal = (person, legalAge) => person.Age >= legalAge;
+
+//Console.WriteLine(isLegal(new Person() { Age = 15 }, 18));
+//class Person
+//{
+//	public int Age { get; set; }
+//}
+
+Action<int, int, int> addNum = (a, b, c) => Console.WriteLine($"{a}, {b}, {c} = {a + b + c}");
+
+addNum(5, 10, 15);
+
+Console.WriteLine();
+
+Func<double, double, double> volymCylinder = (r, h) => double.Round(r * r * Math.PI * h, 2);
+
+Console.WriteLine(volymCylinder(2, 10));
+
+
+
---
+++ L035_Interface/Program.cs
@@ -0,0 +1,105 @@
+﻿
+
+//using (var reader = new StreamReader("myFile.txt"))
+//{
+//    // ...
+//}
+
+//using (var cat = new Cat())
+//{
+//    Console.WriteLine("xXVendiCat1337Xx");
+//}
+//Console.WriteLine("The end!");
+
+
+var myCat = new Cat("Pelle");
+//myCat.Collect();
+
+ICollectable collectable = myCat;
+myCat = (Cat)collectable;
+
+var collectibles = new List<ICollectable>();
+collectibles.Add(myCat);
+collectibles.Add(new PokemonCard());
+
+var myInventory = new Inventory();
+myInventory.TryCollect(new Cat("Måns"));
+if (!myInventory.TryCollect(new PokemonCard()))
+{
+    Console.WriteLine("Could not collect item");
+}
+
+myInventory.Collect(myCat);
+class Inventory
+{
+    public List<ICollectable> Items { get; set; }
+
+    public Inventory()
+    {
+        Items = new List<ICollectable>();
+    }
+    public bool TryCollect(object o)
+    {
+        if (o is ICollectable item)
+        {
+            Items.Add(item);
+            item.Collect();
+            return true;
+        }
+        return false;
+    }
+
+    public void Collect(ICollectable collectable)
+    {
+        Items.Add(collectable);
+    }
+}
+
+class Animal
+{
+    public string Name { get; set; }
+
+    public Animal(string name)
+    {
+        Name = name;
+    }
+}
+
+class Cat : Animal, IDisposable, ICollectable
+{
+
+    public Cat(string name) : base(name){ }
+
+    public void Collect()
+    {
+        Console.WriteLine($"{Name} was collected!");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Disposed!");    
+    }
+
+    public bool IsCollected()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class PokemonCard : ICollectable
+{
+    public void Collect()
+    {
+        Console.WriteLine("You collected a pokemon.");
+    }
+
+    public bool IsCollected()
+    {
+        throw new NotImplementedException();
+    }
+}
+interface ICollectable
+{
+    public void Collect();
+    public bool IsCollected();
+}
