2953041840bc650317f7eeb7b7d22e4e4704b7ed
Melvinedl <medlund01@gmail.com>
2025-10-20 14:34:46 +0200

    L038 L039

---
+++ L038_PLINQ/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+//using System.Diagnostics;
+
+////Process.GetCurrentProcess().ProcessorAffinity = 1;
+
+//var list = Enumerable.Range(1, 50)
+//    .AsParallel()
+//    .Select(i => CostlyOperation(i))
+//    .ToList();
+
+//Console.WriteLine("\nProcessing complete! Here is the result: \n");
+
+//Console.WriteLine(string.Join(", ", list));
+
+
+//static int CostlyOperation(int n)
+//{
+//    //Thread.Sleep(500);
+//    for (int i = 0; i < 1000000000; i++);
+
+//    Console.WriteLine($"Processing object {n}");
+
+//    return n;
+//}
+
+long sum = Enumerable.Range(1, 1_000_000_000)
+    .AsParallel()
+    .Where(i => i % 3 == 0 || i % 5 == 0)
+    .Select(i => (long)i)
+    .Sum();
+
+Console.WriteLine(sum);
+Console.WriteLine();
